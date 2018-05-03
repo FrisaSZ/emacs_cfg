8f383e492e8f3ef2e574b36cd5395ea6b779ce20
侯剑侠 <houjianxia@chelun.com>
2018-05-03 10:06:23 +0800

    sav

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -25,9 +25,10 @@
 ;;(global-hl-line-mode t)
 ;; 使用图形界面时加载主题，使用命令行时不加载
 (if (display-graphic-p)
-    ((message "emacs runs in graphic")
-     (load-theme 'northcode t)
-     (global-hl-line-mode t))
+    (progn
+      (message "emacs runs in graphic")
+      (load-theme 'northcode t)
+      (global-hl-line-mode t))
   (message "emacs runs in terminal")
   (global-linum-mode t)
   (setq linum-format "%d "))
