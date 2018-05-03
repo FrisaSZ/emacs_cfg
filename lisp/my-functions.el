a732bc333239a66883ee403ecb46790631044817
侯剑侠 <houjianxia@chelun.com>
2018-05-03 09:58:46 +0800

    delete semi colon

--- lisp/my-functions.el
+++ lisp/my-functions.el
@@ -1,13 +1,11 @@
 ;; 自定义的函数
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; 打开自己的配置文件
 (defun open-my-init-file()
   (interactive)
   (find-file "~/.emacs.d/init.el"))
 ;; 打开~/.emacs.d/init.el
 (global-set-key (kbd "<f2>") 'open-my-init-file)
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
 ;; provide会把特定的feature添加到features变量中去
