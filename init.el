7f39da9ac7ec709b8d34b39058f44502b32ce498
HouJX <houjianxia@chelun.com>
2018-06-05 14:23:03 +0800

    sav

--- init.el
+++ init.el
@@ -1,13 +1,13 @@
 (package-initialize)
 
 ;; 设置加载路径
-(add-to-list 'load-path "~/.emacs.d/lisp")
+(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 (require 'functions-fsz)
 (require 'packages-fsz)
 (require 'ivy-fsz)
 (require 'builtin-fsz)
 
-(setq custom-file "~/.emacs.d/lisp/emacs-custom.el")
+(setq custom-file (expand-file-name "emacs-custom.el" (expand-file-name "lisp" user-emacs-directory)))
 (load custom-file)
 ;; load 是按照FILENAME去load-path里找，优先找FILENAME.elc，没有的话再去找FILENAME.el，
 ;; load-file是直接按照文件的绝对路径和明确后缀来加载比如~/.emacs.d/init.el
