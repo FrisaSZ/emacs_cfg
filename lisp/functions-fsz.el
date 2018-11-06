aabd94125a08ce051b811b7377cacc0a0f727a9c
HouJX <houjianxia@chelun.com>
2018-11-06 10:19:17 +0800

    use M-<f1> to open my init.el

--- lisp/functions-fsz.el
+++ lisp/functions-fsz.el
@@ -5,7 +5,7 @@
   (interactive)
   (find-file "~/.emacs.d/init.el"))
 ;; 打开~/.emacs.d/init.el
-(global-set-key (kbd "M-<f2>") 'open-my-init-file)
+(global-set-key (kbd "M-<f1>") 'open-my-init-file)
 
 ;; provide会把特定的feature添加到features变量中去
 (provide 'functions-fsz)
