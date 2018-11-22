bc42d5d68616121aadd66a17baf418ee560402df
HouJX <houjianxia@chelun.com>
2018-11-22 10:06:56 +0800

    change recentf location

--- init.el
+++ init.el
@@ -11,3 +11,5 @@
 (load custom-file)
 ;; load 是按照FILENAME去load-path里找，优先找FILENAME.elc，没有的话再去找FILENAME.el，
 ;; load-file是直接按照文件的绝对路径和明确后缀来加载比如~/.emacs.d/init.el
+;; 把最近打开文件的记录保存在HOME目录下
+(setq recentf-save-file (expand-file-name "../emacs-recentf" user-emacs-directory))
